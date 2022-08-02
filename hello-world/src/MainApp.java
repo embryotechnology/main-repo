04784de70b5894ba78d19f5c4abf6fe3c06f4943
embryotechnology <110442415+embryotechnology@users.noreply.github.com>
2022-08-02 09:30:14 +0530

    Add files via upload

---
+++ hello-world/src/MainApp.java
@@ -0,0 +1,19 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		int firstNumber =2;
+		int secondNumber =4;
+		
+		int additionVaue = firstNumber + secondNumber;
+		
+		int multiplicationNumber =3;
+		multiplicationNumber =2;
+		int result = additionVaue * multiplicationNumber;
+		System.out.println(result);
+		
+
+	}
+
+}
